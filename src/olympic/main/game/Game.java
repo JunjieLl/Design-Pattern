46a7adc549c790f282760270983cadf4efd4c755
YWT Wu <wywt789@outlook.com>
2021-10-29 22:51:51 +0800

    athletes

--- src/olympic/main/game/Game.java
+++ src/olympic/main/game/Game.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Game {
-    protected List<Athlete> athleteList = new ArrayList<>();
+    protected List<Athlete> athletes = new ArrayList<>();
     public Game(String name,List<Athlete> athleteList){
-        this.athleteList=athleteList;
+        this.athletes=athleteList;
     }
     abstract public void start();
     protected String name;
