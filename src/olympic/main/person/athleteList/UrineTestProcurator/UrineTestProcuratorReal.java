f230798abc17a3abe9bd900ed6bac0484eea8bf2
ruaner <1763244792@qq.com>
2021-10-30 17:20:15 +0800

    ok

---
+++ src/olympic/main/person/athleteList/UrineTestProcurator/UrineTestProcuratorReal.java
@@ -0,0 +1,20 @@
+package olympic.main.person.athleteList.UrineTestProcurator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class UrineTestProcuratorReal extends UrineTestProcurator {
+	private String grade;
+	private final ArrayList<String> gradeList = new ArrayList<>(Arrays.asList("初级", "中级", "副高级", "高级"));
+	
+	protected UrineTestProcuratorReal(String name, String nation) {
+		super(name, nation);
+		int num = (int) (Math.random() * 4);
+		grade = gradeList.get(num);
+	}
+	
+	@Override
+	public void check() {
+		System.out.println("检察员名为" + name + "，" + "是一位" + grade + "的检察员。");
+	}
+}
