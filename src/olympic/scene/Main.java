1957d82ef0166c591597da0c465a74a6f0519cc1
徐满心 <1426356297@qq.com>
2021-10-31 00:28:53 +0800

    添加两个scene

--- src/olympic/scene/Main.java
+++ src/olympic/scene/Main.java
@@ -2,12 +2,7 @@ package olympic.scene;
 
 public class Main {
     public static void main(String[] args) {
-        Director.getInstance().nextScene(new Scene() {
-            @Override
-            public void play() {
-                System.out.println("奥林匹克运动会开始了！");
-            }
-        });
+        Director.getInstance().nextScene(() -> System.out.println("奥林匹克运动会开始了！"));
         //FootballStarter.start();
 
         Scene divingGame=new DivingGameScene("单人三米");
