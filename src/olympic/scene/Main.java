64d0b5a7b2200caae5b2fdf4823a54ab11c6e8db
Chenhao Zhai <473049712@qq.com>
2021-10-30 22:41:48 +0800

    整合赛前

--- src/olympic/scene/Main.java
+++ src/olympic/scene/Main.java
@@ -1,7 +1,5 @@
 package olympic.scene;
 
-import olympic.main.game.football.FootballStarter;
-
 public class Main {
     public static void main(String[] args) {
         Director.getInstance().nextScene(new Scene() {
@@ -11,7 +9,8 @@ public class Main {
             }
         });
         //FootballStarter.start();
-        Scene divingGame=new DivingGameScene();
+
+        Scene divingGame=new DivingGameScene("单人三米");
         divingGame.play();
         //new CeremonyScene("LI JUNJIE","LI JIEJUN","JIE LIJUN").play();
         //new ChoreHandlingScene().play();
