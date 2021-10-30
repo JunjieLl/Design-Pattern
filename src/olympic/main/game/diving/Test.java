196c89895dd99606719b16506f0b177112e814db
Chenhao Zhai <473049712@qq.com>
2021-10-30 16:00:33 +0800

    删除注释

--- src/olympic/main/game/diving/Test.java
+++ src/olympic/main/game/diving/Test.java
@@ -9,25 +9,8 @@ import java.util.LinkedList;
 
 public class Test {
     public static void main(String[] args) {
-//        LinkedList<Athlete> allMember=new LinkedList<>();
         String context = "下面进行的是单人十米板跳水比赛，请各位运动员做好入场准备";
-//        //单人测试
-//        for (int i = 0; i < 32; i++) {
-//            DivingAthlete divingAthlete = new DivingAthlete("ljj" + i,"中国");
-//            allMember.add(divingAthlete);
-//        }
-        //双人测试
-//        for (int i = 0; i < 64; i+=2) {
-//            LinkedList<Athlete> list=new LinkedList<>();
-//            DivingAthlete divingAthlete1 = new DivingAthlete("ljj" + i,"中国");
-//            DivingAthlete divingAthlete2 = new DivingAthlete("ljj" + (i+1),"中国");
-//            list.add(divingAthlete1);
-//            list.add(divingAthlete2);
-//              DivingTeam athletes =new DivingTeam(i+"组","中国",list);
-//            allMember.add(athletes);
-//        }
-       // PersonFactory.getInstance().getAthletes("DivingTeam");
-        ContestPipeline divingGame=new ContestPipeline(context, PersonFactory.getInstance().getAthletes("DivingTeam"));
+        ContestPipeline divingGame=new ContestPipeline(context, PersonFactory.getInstance().getAthletes("Diving"));
         divingGame.start();
     }
 }
