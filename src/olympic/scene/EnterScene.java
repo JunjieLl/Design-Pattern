8bac10192790e8c1d6262c75305f08d10c0501ee
yang yuqing <957890361@qq.com>
2021-10-31 16:01:30 +0800

    Update EnterScene.java

--- src/olympic/scene/EnterScene.java
+++ src/olympic/scene/EnterScene.java
@@ -15,23 +15,26 @@ public class EnterScene implements Scene {
         String[] country=new String[48];
         country= PersonFactory.getInstance().getNations().toArray(new String[0]);
 
-        for(int i=0;i<48;i++) {
-            if (i % 3 == 0) {
-                TeamAthlete tempList = new TeamAthlete(PersonFactory.getInstance().getAthleteByNation(country[i]), country[i]);
-                if (tempList.getNumber() > 0) {
-                    enterManager.setTeams(tempList);
-                    if (i % 6 == 0) {
-                        torchBarer.addMember(tempList.getFirstAthlete());
-                    }
-                }
+        for(int i=0;i<10;i++)
+        {
+            int ran= 0+(int)(Math.random()*(47-0+1));
+
+            TeamAthlete tempList = new TeamAthlete(PersonFactory.getInstance().getAthleteByNation(country[ran]),country[ran]);
+            if (tempList.getNumber() > 0) {
+                enterManager.setTeams(tempList);
+                torchBarer.addMember(tempList.getFirstAthlete());
             }
-        }
+
+        };
+
         EnterIterator et = enterManager.iterator();
         while(et.hasNext()) {
             TeamAthlete myList=et.next();
             System.out.println("现在向我们走来的是"+myList.getNation()+"队，让我们欢迎他们！");
         }
+        System.out.println('\n');
         torchBarer.passFire();
+        System.out.println("最后一位使者点燃了奥运圣火！");
     }
 
 
