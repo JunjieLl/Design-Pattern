67ef3bb45b78bfff4647a73d8476d1e36342e1a0
李俊杰 <1061455426@qq.com>
2021-11-04 13:39:43 +0800

    Update Country.java

--- src/main/java/olympic/main/postgame/medaltable/Country.java
+++ src/main/java/olympic/main/postgame/medaltable/Country.java
@@ -5,7 +5,7 @@ public class Country implements Comparable<Country> {
     private int goldMedalNum;
     private int silverMedalNum;
     private int bronzeMedalNum;
-    private String countryCode;
+    private final String countryCode;
 
     public Country(int goldMedalNum, int silverMedalNum, int bronzeMedalNum, String countryCode) {
         this.bronzeMedalNum = bronzeMedalNum;
@@ -31,15 +31,6 @@ public class Country implements Comparable<Country> {
     }
 
 
-    private int compareTo(String name2, String name3) {
-        int s, i = 0;
-        if ((s = (name3.charAt(i) - name2.charAt(i))) != 0) {
-            return s;
-        } else {
-            i++;
-        }
-        return 0;
-    }
 
     public String getCountryCode() {
         return this.countryCode;
