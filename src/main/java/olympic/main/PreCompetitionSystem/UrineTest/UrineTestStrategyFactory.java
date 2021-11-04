590c1b4fc447f6544ab92aad81894610629850d5
ruaner <1763244792@qq.com>
2021-11-04 11:28:02 +0800

    规范了输出

--- src/main/java/olympic/main/PreCompetitionSystem/UrineTest/UrineTestStrategyFactory.java
+++ src/main/java/olympic/main/PreCompetitionSystem/UrineTest/UrineTestStrategyFactory.java
@@ -11,33 +11,37 @@ public class UrineTestStrategyFactory {
      * 策略模式的对象池，用于存储策略、共享对象
      */
     private final HashMap<Integer, UrineTestStrategy> pool = new HashMap<>();
-
+    
     /**
      * 空策略对象，当用户想要的尿检策略不存在时会返回该值
      */
     private final UrineTestStrategy nullStrategy = new NullUrineTestTechnologyStrategy();
-
+    
     /**
      * 尿检策略工厂的单例，用于保证所有类共享一个尿检策略工厂
      */
     private static final UrineTestStrategyFactory singleton = new UrineTestStrategyFactory();
-
+    
     /**
      * 尿检策略工厂的私有构造函数，是单例模式的一部分
      */
     private UrineTestStrategyFactory() {
-
+    
     }
-
+    
     /**
      * 获取尿检策略工厂单例
      *
+     * @param isPrint 控制是否输出classname语句
      * @return 尿检策略工厂单例
      */
-    public static UrineTestStrategyFactory getInstance() {
+    public static UrineTestStrategyFactory getInstance(boolean isPrint) {
+        if(isPrint) {
+            System.out.println("classname: (UrineTestStrategyFactory) method: (getInstance) " + "action: (给出尿检策略的工厂，在工厂中可以共享那些策略，使用了Fly weight模式以及Factory模式) ");
+        }
         return singleton;
     }
-
+    
     /**
      * 根据输入的数字获取具体的尿检策略，如果不存在该尿检策略则返回空尿检策略
      *
@@ -59,5 +63,5 @@ public class UrineTestStrategyFactory {
         }
         return urineTestStrategy;
     }
-
+    
 }
