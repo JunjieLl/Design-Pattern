9f3899055738f4b3ce79a4bce97f6ff701b146c4
zhangbaiyi <zbaiyi@outlook.com>
2021-11-06 03:14:11 +0800

    火速修bug

--- src/main/java/olympic/scene/ReviewQScene.java
+++ src/main/java/olympic/scene/ReviewQScene.java
@@ -3,6 +3,7 @@ package olympic.scene;
 import olympic.main.person.PersonFactory;
 import olympic.main.person.athlete.Athlete;
 import olympic.main.postgame.reviewqualification.ReviewMediatorConcrete;
+import olympic.utils.PrintBlockFormat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,37 +20,39 @@ public class ReviewQScene implements Scene {
     @Override
     public void play() {
         List<String> games = PersonFactory.getInstance().getNames();
-
-        List<String> individualGames = PersonFactory.getInstance().getSingalGameNames();
-
-        int currentGameIndex = new Random().nextInt(individualGames.size());
-
-        String targetGameName = individualGames.get(currentGameIndex);
-
-        List<Athlete> reviewee = PersonFactory.getInstance().getAthletes(targetGameName);
-
-        ArrayList<Athlete> topAthletes = new ArrayList<>();
-
-        ArrayList<String> PingpongDiving = new ArrayList<>();
-        PingpongDiving.add("Pingpong");
-        PingpongDiving.add("Diving");
-        PingpongDiving.add("Badminton");
-
-
-        if (PingpongDiving.contains(targetGameName)) {
-            for (Athlete athlete : reviewee) {
-                if (athlete.getRank("决赛") < 4 && athlete.getRank("决赛") > 0) {
-                    topAthletes.add(athlete);
-                }
-            }
-        } else {
-            for (Athlete athlete : reviewee) {
-                if (athlete.getRank(targetGameName) < 4 && athlete.getRank(targetGameName) > 0) {
-                    topAthletes.add(athlete);
-                }
+        ArrayList<String> individualGames = new ArrayList<String>();
+        ArrayList<Integer> TeamIndex = new ArrayList<>();
+        for (String game : games) {
+            if (!game.endsWith("Team")) {
+                individualGames.add(new String(game));
             }
         }
-        Athlete badAthlete = topAthletes.get(new Random().nextInt(topAthletes.size()));
-        new ReviewMediatorConcrete(badAthlete, individualGames.get(currentGameIndex));
+
+        String[] individuals = {"Marathon", "Hurdling", "Sprints"};
+        int currentGameIndex = new Random().nextInt(individuals.length);
+        List<Athlete> reviewee = PersonFactory.getInstance().getAthletes(individuals[currentGameIndex]);
+
+        ArrayList<Athlete> topAthletes = new ArrayList<>();
+        String currentGameRankKey = individuals[currentGameIndex];
+
+        for (Athlete athlete : reviewee) {
+            if (athlete.getRank(currentGameRankKey) < 4 && athlete.getRank(currentGameRankKey) > 0) {
+                topAthletes.add(athlete);
+            }
+        }
+
+        if(topAthletes.size() != 0){
+            Athlete badAthlete = topAthletes.get(new Random().nextInt(topAthletes.size()));
+            new ReviewMediatorConcrete(badAthlete, individualGames.get(currentGameIndex));
+        }
+        else{
+            List<String> noProblemPrintBlock = new ArrayList<>();
+            noProblemPrintBlock.add("调查管理员");
+            noProblemPrintBlock.add("没有收到对比赛公正性的举报，在今后本委员会会一直监控比赛情况。");
+            PrintBlockFormat printBlockFormat = PrintBlockFormat.getPrintFormat();
+            printBlockFormat.printFormatLeftScreen(noProblemPrintBlock, true);
+        }
+
+
     }
 }
