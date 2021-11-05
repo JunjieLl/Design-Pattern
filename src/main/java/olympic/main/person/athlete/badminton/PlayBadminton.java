315079d24a428656b18ed15d77b7eb1b9bf224c4
李俊杰 <1061455426@qq.com>
2021-11-05 23:46:31 +0800

    delete

--- src/main/java/olympic/main/person/athlete/badminton/PlayBadminton.java
+++ src/main/java/olympic/main/person/athlete/badminton/PlayBadminton.java
@@ -12,17 +12,17 @@ public interface PlayBadminton extends CallBack {
      * @param oppoent 对手
      * @return 是否打赢
      */
-    public Boolean playWith(CallBack oppoent);
+Boolean playWith(CallBack oppoent);
 
     /**
      * 发球
      */
-    public void serve();
+void serve();
 
     /**
      * 返回运动员的实力
      * @return 实力
      */
-    public Integer getCapacity();
+ Integer getCapacity();
 
 }
