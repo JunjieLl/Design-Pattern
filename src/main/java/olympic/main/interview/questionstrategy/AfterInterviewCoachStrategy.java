573152c3e3d2c97fb935d867b48e2e4e3cf6c8d9
徐满心 <1426356297@qq.com>
2021-11-06 01:20:39 +0800

    添加注释

--- src/main/java/olympic/main/interview/questionstrategy/AfterInterviewCoachStrategy.java
+++ src/main/java/olympic/main/interview/questionstrategy/AfterInterviewCoachStrategy.java
@@ -1,7 +1,12 @@
 package olympic.main.interview.questionstrategy;
 
+/**
+ * 赛后采访教练Strategy
+ */
 public class AfterInterviewCoachStrategy extends InterviewStrategy{
-
+    /**
+     * 赛后采访教练所有可能问的问题
+     */
     private static final String[] allPossibleQuestions = {
             "给大家打个招呼吧！",
             "觉得今天自己执教的队员们发挥得怎样？",
@@ -10,6 +15,9 @@ public class AfterInterviewCoachStrategy extends InterviewStrategy{
             "现在心情怎么样？"
     };
 
+    /**
+     * 构建赛后采访教练Strategy
+     */
     public AfterInterviewCoachStrategy() {
         super();
         prepareQuestions(allPossibleQuestions);
