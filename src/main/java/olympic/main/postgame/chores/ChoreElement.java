d81a70ce786e3a789a59bf70890888c65cf4f089
zhangbaiyi <zbaiyi@outlook.com>
2021-11-05 23:14:59 +0800

    注释

--- src/main/java/olympic/main/postgame/chores/ChoreElement.java
+++ src/main/java/olympic/main/postgame/chores/ChoreElement.java
@@ -4,5 +4,8 @@ package olympic.main.postgame.chores;
  * 表示杂务数据结构的接口，接受访问者访问.
  */
 public interface ChoreElement {
+    /**
+     * @param v 当前的访问者
+     */
     public abstract void accept(ChoreVisitor v);
 }
